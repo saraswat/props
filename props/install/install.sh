42f63e77abce7852feb8a090bc19782a06e4a42e
Vijay Saraswat <vsaraswa@us.ibm.com>
2017-06-17 19:48:52 -0400

    Changes needed to get this to build on macos. Note also the change needed in JPype. Add the line "        /System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/" at the end of setupInclusion(self).

--- props/install/install.sh
+++ props/install/install.sh
@@ -7,18 +7,18 @@ fi
 mkdir -p ./berkeleyparser/
 wget https://github.com/emcnany/berkeleyinterface/archive/master.zip -O ./install/berekeleyinterface.zip
 wget https://github.com/astraw/svg_stack/archive/master.zip -O install/svg_stack.zip
-wget "http://downloads.sourceforge.net/project/jpype/JPype/0.5.4/JPype-0.5.4.2.zip?r=&ts=1459330071&use_mirror=nchc" -O install/JPype-0.5.4.2.zip
+#wget "http://downloads.sourceforge.net/project/jpype/JPype/0.5.4/JPype-0.5.4.2.zip?r=&ts=1459330071&use_mirror=nchc" -O install/JPype-0.5.4.2.zip
 wget "https://github.com/slavpetrov/berkeleyparser/raw/master/eng_sm6.gr" -O ./berkeleyparser/eng_sm6.gr
 wget "https://github.com/slavpetrov/berkeleyparser/raw/master/BerkeleyParser-1.7.jar" -O ./berkeleyparser/BerkeleyParser-1.7.jar
 wget https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.1/stanford-corenlp-3.3.1.jar -O ./dependency_tree/stanford-corenlp-3.3.1.jar
 cd install/
 unzip -o svg_stack.zip 
 unzip -o berekeleyinterface.zip
-unzip -o JPype-0.5.4.2.zip
+#unzip -o JPype-0.5.4.2.zip
 cd berkeleyinterface-master/
 # fix version compatibility issue between PropS, Berkeley Interface and setuptools
 # permanent fix should happen in Berkeley Interface repository, temporary fix until then:
-sed -i s/six==1.5.2/six==1.10.0/ requirements.txt
+#vj sed -i s/six==1.5.2/six==1.10.0/ requirements.txt
 python setup.py install
 cd ../svg_stack-master/
 python setup.py install
